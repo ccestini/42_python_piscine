e3832e9bb1c3cce7a18b65810e94cf36bc679406
ccestini <99199858+ccestini@users.noreply.github.com>
2024-06-30 13:00:47 +0000

    p03 ex04 done

--- Python_3_OOP/ex03/ft_calculator.py
+++ Python_3_OOP/ex03/ft_calculator.py
@@ -3,38 +3,38 @@ class calculator:
     """Calculator to perform arithmetic operations (+ - * /) on a vector."""
     def __init__(self, vector):
         """Calculator Constructor"""
-        if not all(isinstance(x, (int, float)) for x in vector):
-            raise ValueError("Vector must contain only numbers (int/floats)")
+        # if not all(isinstance(x, (int, float)) for x in vector):
+        #     raise ValueError("Vector must contain only numbers (int/floats)")
         self.vector = vector
 
     def __add__(self, object) -> None:
         """Adds a scalar (object) to each element of the vector"""
-        if not isinstance(object, (int, float)):
-            raise ValueError("The scalar must be a number")
+        # if not isinstance(object, (int, float)):
+        #     raise ValueError("The scalar must be a number")
         result_vector = [x + object for x in self.vector]
         # list comprehension that iterates over each element x in vector
         print(result_vector)
 
     def __mul__(self, object):
         """"Multiplies each elem of the vector by a scalar (object)"""
-        if not isinstance(object, (int, float)):
-            raise ValueError("The scalar must be a number")
+        # if not isinstance(object, (int, float)):
+        #     raise ValueError("The scalar must be a number")
         result_vector = [x * object for x in self.vector]
         print(result_vector)
 
     def __sub__(self, object):
         """Subtracts a scalar (object) from each element of the vector"""
-        if not isinstance(object, (int, float)):
-            raise ValueError("The scalar must be a number")
+        # if not isinstance(object, (int, float)):
+        #     raise ValueError("The scalar must be a number")
         result_vector = [x - object for x in self.vector]
         print(result_vector)
 
     def __truediv__(self, object):
         """Divides each element of the vector by a scalar (object)"""
-        if not isinstance(object, (int, float)):
-            raise ValueError("The scalar must be a number")
-        if object == 0:
-            raise ZeroDivisionError("Division by zero is not allowed")
+        # if not isinstance(object, (int, float)):
+        #     raise ValueError("The scalar must be a number")
+        # if object == 0:
+        #     raise ZeroDivisionError("Division by zero is not allowed")
         result_vector = [x / object for x in self.vector]
         print(result_vector)
 
---
+++ Python_3_OOP/ex04/ft_calculator.py
@@ -0,0 +1,69 @@
+class calculator:
+    """
+    Perform calculations (dot product, addition, subtraction) on vectors.
+    """
+
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """
+        Computes the dot product of two vectors and prints the result.
+        """
+        result_list = [(x * y) for x, y in zip(V1, V2)]
+        result = sum(result_list)
+        print(f"Dot product is: {result}")
+
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """
+        Adds two vectors element-wise and prints the result.
+        """
+        result = [float(x + y) for x, y in zip(V1, V2)]
+        print(f"Add Vector is: {result}")
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """
+        Subtracts the second from the first vector and prints the result.
+        """
+        result = [float(x - y) for x, y in zip(V1, V2)]
+        print(f"Sous Vector is: {result}")
+
+
+"""
+@staticmethod makes it clear that the methods do not depend on the class or
+ instance state and are purely utility functions that operate only on
+ their input arguments.
+
+zip() function:
+Takes 2 iterables and returns an iterator of tuples.
+Each tuple contains elems from the iterables that are at the same position.
+V1 = [5, 10, 2]
+V2 = [2, 4, 3]
+list(zip(V1, V2))  # [(5, 2), (10, 4), (2, 3)]
+List Comprehension:
+The list comprehension iterates over the tuples produced by zip(V1, V2).
+For each tuple (x, y), where x is from V1 and y is from V2, it computes x + y.
+The results from each iteration are collected into a new list.
+The new list [7, 14, 5].
+"""
+
+
+def main():
+    try:
+        a = [2, 2, 2.0, 3, 5]
+        b = [1, 1, 1, 2, 3]
+
+        calculator.dotproduct(a, b)
+        print("---")
+
+        calculator.add_vec(a, b)
+        print("---")
+
+        calculator.sous_vec(a, b)
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+if __name__ == "__main__":
+    main()
