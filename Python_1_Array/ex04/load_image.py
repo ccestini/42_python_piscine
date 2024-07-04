915025549c3e0b7ced4ba509e0b3b218d6d8a99c
ccestini <99199858+ccestini@users.noreply.github.com>
2024-07-04 19:21:37 +0000

    p1 revision2

--- Python_1_Array/ex04/load_image.py
+++ Python_1_Array/ex04/load_image.py
@@ -13,13 +13,41 @@ def ft_load(path: str) -> np.array:
     np.array: The image data in RGB format.
     """
     try:
+
+        # Open the image file, with a context manager, which is a way to
+        # ensure that resources are properly managed and cleaned up after use
+        # not having to use img.close().
         with Image.open(path) as img:
+            # Check if the image format is supported, not all formats
+            # are supported by pillow package so I left only required in subj
             if img.format not in ['JPEG', 'JPG']:
                 raise ValueError(f"Unsupported image format: {img.format}")
+
+            # Convert image to RGB format
             img_rgb = img.convert('RGB')
+
+            # Convert the image data to a NumPy array
             img_array = np.array(img_rgb)
+
+            # Print the shape of the image
+            print(f"The shape of image is: {img_array.shape}")
+
+            # Return the image data
             return img_array
+
     except FileNotFoundError:
         raise ValueError(f"File not found: {path}")
     except Exception as e:
-        raise ValueError(f"An error occurred during loading the image: {e}")
+        raise ValueError(f"An error occurred: {e}")
+
+
+def main():
+    """Main function to test."""
+    try:
+        print(ft_load("../landscape.jpg"))
+    except Exception as message:
+        print(f"{type(message).__name__}: {message}")
+
+
+if __name__ == "__main__":
+    main()
