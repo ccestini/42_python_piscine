5f6471d5c28ed70c6bea58db1f59b55435e1beb4
ccestini <99199858+ccestini@users.noreply.github.com>
2024-07-08 15:40:16 +0000

    pronto para avaliacao

--- Python_3_OOP/ex03/ft_calculator.py
+++ Python_3_OOP/ex03/ft_calculator.py
@@ -3,40 +3,30 @@ class calculator:
     """Calculator to perform arithmetic operations (+ - * /) on a vector."""
     def __init__(self, vector):
         """Calculator Constructor"""
-        # if not all(isinstance(x, (int, float)) for x in vector):
-        #     raise ValueError("Vector must contain only numbers (int/floats)")
         self.vector = vector
 
     def __add__(self, object) -> None:
         """Adds a scalar (object) to each element of the vector"""
-        # if not isinstance(object, (int, float)):
-        #     raise ValueError("The scalar must be a number")
-        result_vector = [x + object for x in self.vector]
+        self.vector = [x + object for x in self.vector]
         # list comprehension that iterates over each element x in vector
-        print(result_vector)
+        print(self.vector)
 
     def __mul__(self, object):
         """"Multiplies each elem of the vector by a scalar (object)"""
-        # if not isinstance(object, (int, float)):
-        #     raise ValueError("The scalar must be a number")
-        result_vector = [x * object for x in self.vector]
-        print(result_vector)
+        self.vector = [x * object for x in self.vector]
+        print(self.vector)
 
     def __sub__(self, object):
         """Subtracts a scalar (object) from each element of the vector"""
-        # if not isinstance(object, (int, float)):
-        #     raise ValueError("The scalar must be a number")
-        result_vector = [x - object for x in self.vector]
-        print(result_vector)
+        self.vector = [x - object for x in self.vector]
+        print(self.vector)
 
     def __truediv__(self, object):
         """Divides each element of the vector by a scalar (object)"""
-        # if not isinstance(object, (int, float)):
-        #     raise ValueError("The scalar must be a number")
-        # if object == 0:
-        #     raise ZeroDivisionError("Division by zero is not allowed")
-        result_vector = [x / object for x in self.vector]
-        print(result_vector)
+        if object == 0:
+            raise ZeroDivisionError("Division by zero is not allowed")
+        self.vector = [x / object for x in self.vector]
+        print(self.vector)
 
 
 """
@@ -67,7 +57,7 @@ def main():
         print("---")
 
         v4 = calculator([10, 15, 20.0])
-        v4 / 5
+        v4 / 0
 
     except Exception as e:
         print(f"Exception: {e}")
