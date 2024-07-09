a02400271cda8fff359079d457760e4d810fb5b6
ccestini <99199858+ccestini@users.noreply.github.com>
2024-07-09 20:29:22 +0000

    finished all

--- Python_4_Dod/ex02/callLimit.py
+++ Python_4_Dod/ex02/callLimit.py
@@ -4,37 +4,18 @@ from typing import Any
 def callLimit(limit: int):
     """
     A decorator to limit the number of times a function can be called.
-
-    Args:
-        limit (int): The maximum number of allowed calls.
-
-    Returns:
-        function: The decorated function with a call limit.
     """
     count = 0
 
     def callLimiter(function):
         """
         Inner decorator function to apply the call limit.
-
-        Args:
-            function: The function to be limited.
-
-        Returns:
-            function: The wrapped function with call limit logic.
         """
         def limit_function(*args: Any, **kwargs: Any):
             """
             Function to check the call limit and execute the function.
-
-            Args:
-                *args: Positional arguments for the wrapped function.
-                **kwargs: Keyword arguments for the wrapped function.
-
-            Returns:
-                Any: The return value of the wrapped function.
             """
-            nonlocal count
+            nonlocal count  # indicates that count is not local variable
             if count < limit:
                 count += 1
                 return function(*args, **kwargs)
@@ -50,23 +31,22 @@ def callLimit(limit: int):
 Create a decorator callLimit that restricts the number of times a function
  can be called. The decorator will count the number of calls to the
  decorated function and raise an error if the limit is exceeded.
+
+Wrapper generally refers to a design pattern where one function or class
+ method modifies the behavior of another function or method. This is
+ achieved using decorators or by wrapping one function within another.
 """
 
 
 def main():
     """Main function to test."""
     try:
-        @callLimit(3)
-        def f():
-            print("f()")
+        @callLimit(2)
+        def example_function():
+            print("Example function")
 
-        @callLimit(1)
-        def g():
-            print("g()")
-
-        for i in range(3):
-            f()
-            g()
+        for i in range(5):
+            example_function()
 
     except Exception as msg:
         print(f"An error occurred: {msg}")
